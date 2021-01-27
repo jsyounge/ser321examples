2e61b9eb660388e7b141e70804b3c2e06b2e1205
jsyounge <77308314+jsyounge@users.noreply.github.com>
2021-01-26 21:23:21 -0500

    Update WebServer.java

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -8,6 +8,7 @@ You can also do some other simple GET requests:
 4) /multiply?num1=3&num2=4 multiplies the two inputs and responses with the result
 5) /github?query=users/amehlhase316/repos (or other GitHub repo owners) will lead to receiving
    JSON which will for now only be printed in the console. See the todo below
+
 The reading of the request is done "manually", meaning no library that helps making things a 
 little easier is used. This is done so you see exactly how to pars the request and 
 write a response back
@@ -398,3 +399,4 @@ class WebServer {
     }
     return sb.toString();
   }
+}
